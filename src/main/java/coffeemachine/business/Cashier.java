63560c672cd3c2e1b1f1a51c39d18d3a083b4582
arebai <achrafrebai123@gmail.com>
2020-01-12 16:26:00 +0100

    refactoring : introduce <Drink,Price> Map

--- src/main/java/coffeemachine/business/Cashier.java
+++ src/main/java/coffeemachine/business/Cashier.java
@@ -2,9 +2,21 @@ package coffeemachine.business;
 
 import coffeemachine.Drink;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cashier {
+
     public Money checkout(Drink drink, Money money) {
-        double drinkPrice = drink == Drink.TEA ? 0.4 : 0.6;
-        return new Money(drinkPrice - money.getAmount());
+        Map<Drink, Money> priceByDrink = getDrinksPrice();
+        Money drinkPrice = priceByDrink.get(drink);
+        return new Money(drinkPrice.getAmount() - money.getAmount());
+    }
+
+    private Map<Drink, Money> getDrinksPrice() {
+        Map<Drink, Money> priceByDrink = new HashMap<Drink, Money>();
+        priceByDrink.put(Drink.TEA, new Money(0.4));
+        priceByDrink.put(Drink.COFFEE, new Money(0.6));
+        return priceByDrink;
     }
 }
