53f07c0f442a2efb3128ab24c6f9cb098300c260
arebai <achrafrebai123@gmail.com>
2020-01-12 16:17:32 +0100

    compute coffee missing money

--- src/main/java/coffeemachine/business/Cashier.java
+++ src/main/java/coffeemachine/business/Cashier.java
@@ -4,7 +4,7 @@ import coffeemachine.Drink;
 
 public class Cashier {
     public Money checkout(Drink drink, Money money) {
-        double teaPrice = 0.4;
-        return new Money(teaPrice - money.getAmount());
+        double drinkPrice = drink == Drink.TEA ? 0.4 : 0.6;
+        return new Money(drinkPrice - money.getAmount());
     }
 }
--- src/test/java/coffeemachine/business/CashierTest.java
+++ src/test/java/coffeemachine/business/CashierTest.java
@@ -9,13 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CashierTest {
 
     private static Offset<Double> OFFSET = Offset.offset(0.0000001);
+    private Cashier cashier = new Cashier();
 
     @Test
-    public void should_return_tea_missing_money() {
-        Cashier cashier = new Cashier();
+    public void should_compute_tea_missing_money() {
         assertThat(cashier.checkout(Drink.TEA, new Money(0.3)).getAmount()).isCloseTo(new Money(0.1).getAmount(), OFFSET);
         assertThat(cashier.checkout(Drink.TEA, new Money(0.1)).getAmount()).isCloseTo(new Money(0.3).getAmount(), OFFSET);
         assertThat(cashier.checkout(Drink.TEA, new Money(0.2)).getAmount()).isCloseTo(new Money(0.2).getAmount(), OFFSET);
+    }
+
+    @Test
+    public void should_compute_coffee_missing_money() {
+        assertThat(cashier.checkout(Drink.COFFEE, new Money(0.4)).getAmount()).isCloseTo(new Money(0.2).getAmount(), OFFSET);
     }
 
 
