ac1457f14220bb3572510972eb7b35a0b01eb2a6
arebai <achrafrebai123@gmail.com>
2020-01-12 17:34:05 +0100

    Ability to support extra hot drinks

--- src/main/java/coffeemachine/Order.java
+++ src/main/java/coffeemachine/Order.java
@@ -4,9 +4,11 @@ public class Order {
 
     private final Drink drink;
     private final int sugarAmount;
+    private final boolean extraHot;
 
-    public Order(Drink drink, int sugarAmount) {
+    public Order(Drink drink, boolean extraHot, int sugarAmount) {
         this.drink = drink;
+        this.extraHot = extraHot;
         this.sugarAmount = sugarAmount;
     }
 
@@ -20,5 +22,9 @@ public class Order {
 
     public boolean withStick() {
         return sugarAmount >= 1;
+    }
+
+    public boolean isExtraHot() {
+        return extraHot;
     }
 }
--- src/test/java/coffeemachine/OrderTest.java
+++ src/test/java/coffeemachine/OrderTest.java
@@ -16,35 +16,38 @@ public class OrderTest {
     private Drink drink;
     private int sugarAmount;
     private boolean withStick;
+    private boolean extraHot;
 
     @Parameters
     public static Collection<Object[]> params() {
         return Arrays.asList(
                 new Object[][]{
-                        {Drink.TEA, 1, true},
-                        {Drink.TEA, 0, false},
-                        {Drink.CHOCOLATE, 0, false},
-                        {Drink.CHOCOLATE, 1, true},
-                        {Drink.COFFEE, 2, true},
-                        {Drink.COFFEE, 4, true},
-                        {Drink.COFFEE, 0, false},
-                        {Drink.JUICE, 0, false},
+                        {Drink.TEA, false, 1, true},
+                        {Drink.TEA, false, 0, false},
+                        {Drink.CHOCOLATE, false, 0, false},
+                        {Drink.CHOCOLATE, true, 1, true},
+                        {Drink.COFFEE, false, 2, true},
+                        {Drink.COFFEE, false, 4, true},
+                        {Drink.COFFEE, true, 0, false},
+                        {Drink.JUICE, false, 0, false},
 
                 }
         );
     }
 
-    public OrderTest(Drink drink, int sugarAmount, boolean withStick) {
+    public OrderTest(Drink drink, boolean extraHot, int sugarAmount, boolean withStick) {
         this.drink = drink;
         this.sugarAmount = sugarAmount;
         this.withStick = withStick;
+        this.extraHot = extraHot;
     }
 
     @Test
     public void should_create_order() {
-        Order order = new Order(drink, sugarAmount);
+        Order order = new Order(drink, extraHot, sugarAmount);
         assertThat(order.getDrink()).isEqualTo(drink);
         assertThat(order.getSugarAmount()).isEqualTo(sugarAmount);
         assertThat(order.withStick()).isEqualTo(withStick);
+        assertThat(order.isExtraHot()).isEqualTo(extraHot);
     }
 }
