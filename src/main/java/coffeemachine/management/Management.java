8e77bded2a32632679abb4ef92ba8c04d23e3990
arebai <achrafrebai123@gmail.com>
2020-01-12 20:35:36 +0100

    compute report from different drinks ordered

--- src/main/java/coffeemachine/management/Management.java
+++ src/main/java/coffeemachine/management/Management.java
@@ -1,6 +1,7 @@
 package coffeemachine.management;
 
 import coffeemachine.Drink;
+import coffeemachine.business.Menu;
 import coffeemachine.business.Money;
 
 import java.util.Collection;
@@ -11,15 +12,30 @@ import java.util.stream.Collectors;
 public class Management {
 
     private OrderRepository orderRepository;
+    private Menu menu;
 
-    public Management(OrderRepository orderRepository) {
+    public Management(OrderRepository orderRepository, Menu menu) {
         this.orderRepository = orderRepository;
+        this.menu = menu;
     }
 
     public Report generate() {
         Collection<Drink> drinks = orderRepository.getDailyDrinks();
         Map<Drink, Long> countByDrink = getCountByDrink(drinks);
-        return new Report(countByDrink, new Money(0.4));
+        return new Report(countByDrink, getTotalEarned(countByDrink));
+    }
+
+    private Money getTotalEarned(Map<Drink, Long> countByDrink) {
+        double totalEarned = countByDrink.entrySet()
+                .stream()
+                .map(drinkCountEntry -> getEarnedForDrink(drinkCountEntry.getKey(), drinkCountEntry.getValue()))
+                .mapToDouble(Money::getAmount).sum();
+        return new Money(totalEarned);
+    }
+
+    private Money getEarnedForDrink(Drink drink, Long orderCount) {
+        Money price = menu.getPrice(drink);
+        return new Money(orderCount * price.getAmount());
     }
 
     private Map<Drink, Long> getCountByDrink(Collection<Drink> drinks) {
--- src/test/java/coffeemachine/management/ManagementTest.java
+++ src/test/java/coffeemachine/management/ManagementTest.java
@@ -1,9 +1,12 @@
 package coffeemachine.management;
 
 import coffeemachine.Drink;
+import coffeemachine.business.Menu;
 import coffeemachine.business.Money;
+import coffeemachine.business.impl.MenuMemoryImpl;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
@@ -11,13 +14,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ManagementTest {
 
+    private static final Menu MENU = new MenuMemoryImpl();
+
     @Test
     public void should_generate_report_with_tea_count() {
-        Management management = new Management(() -> Collections.singletonList(Drink.TEA));
+        Management management = new Management(() -> Collections.singletonList(Drink.TEA), MENU);
         Report report = management.generate();
         Map<Drink, Long> countByDrink = report.getCountByDrink();
         assertThat(countByDrink.get(Drink.TEA)).isEqualTo(1);
         assertThat(report.getTotalEarned()).isEqualTo(new Money(0.4));
+    }
+
+    @Test
+    public void should_generate_report_when_pass_two_tea_one_juice() {
+        // given
+        Management management = new Management(() -> Arrays.asList(Drink.TEA, Drink.JUICE, Drink.TEA), MENU);
+
+        // when
+        Report report = management.generate();
+
+        // then
+        Map<Drink, Long> countByDrink = report.getCountByDrink();
+        assertThat(countByDrink.get(Drink.TEA)).isEqualTo(2);
+        assertThat(countByDrink.get(Drink.JUICE)).isEqualTo(1);
+        assertThat(report.getTotalEarned()).isEqualTo(new Money(1.4));
     }
 
 }
